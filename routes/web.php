03467716a20ebc176ad49ac082f0d2305243b135
Elite Dev Emon <elitedevemon@gmail.com>
2024-10-28 22:17:53 +0600

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+  return view('welcome');
+});
+
+//optional parameter route
+Route::get('post/{id?}', function (string $id=null) {
+  if (!empty($id)) {
+    return $id;
+  }
+  return "No id found";
+});
+
+//parameter type selection
+Route::get('post/slug/{id?}', function(string $id=null){
+  if (!empty($id)) {
+    return $id;
+  }
+  return "No id found";
+})->whereAlpha('id');
